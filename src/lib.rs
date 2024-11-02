7c6d26c8ba0124d52e7da39dd6679b97aebedfc7
HsiangNianian <admin@asynth.in>
2024-11-03 02:53:35 +0800

    feat(LibCore): add raw_input function and update entry point

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+
 use pyo3::prelude::*;
 
 pub mod lex;
@@ -12,9 +16,19 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+#[pyfunction]
+fn raw_input(prompt: String) -> PyResult<()> {
+    let file = File::open(&prompt).unwrap();
+
+    let proto = parse::load(BufReader::new(file));
+    vm::ExeState::new().execute(&proto, &Vec::new());
+    Ok(())
+}
+
 #[pymodule]
 #[pyo3(name = "LibCore")]
 fn libcore(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(raw_input, m)?)?;
     Ok(())
 }
