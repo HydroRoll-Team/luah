e5b9c92cd48720ec938f90a79f55a0c0e68645a7
HsiangNianian <admin@asynth.in>
2024-11-03 02:32:36 +0800

    feat(bytecode, lex, parse): enhance bytecode, lex, and parse modules with new features and optimizations

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
 use pyo3::prelude::*;
 
+pub mod lex;
+pub mod parse;
+pub mod bytecode;
+pub mod vm;
+pub mod value;
+pub mod utils;
+
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
--- tests/hello_world.lua
+++ tests/hello_world.lua
@@ -1,3 +1,3 @@
 print "hello, world!"
-print "hi"
+print "你好"
 print "hi, 😀"
